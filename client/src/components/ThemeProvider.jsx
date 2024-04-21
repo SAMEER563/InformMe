ba2bb08a2776c8bc37dea0684005632a7a8f465e
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-21 18:43:42 +0530

    make dashbaord private

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux"
 
-export default function ThemeProvider({children}) {
+export default function ThemeProvider({ children }) {
     const {theme} = useSelector(state => state.theme)
   return (
     <div className={theme}>
@@ -8,5 +8,5 @@ export default function ThemeProvider({children}) {
         {children}
       </div>
     </div>
-  )
+  );
 }
