00962e030436efdc0f70d32c8a4e9784bdbc44f1
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-05-04 23:21:25 +0530

    post model change

--- api/models/post.model.js
+++ api/models/post.model.js
@@ -19,9 +19,17 @@ const postSchema = new mongoose.Schema(
     image: {
       type: String,
       default:
-        'https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png',
+        'https://images.static-collegedunia.com/public/college_data/images/campusimage/151937286431.JPG',
     },
     category: {
+      type: String,
+      default: 'uncategorized',
+    },
+    course: {
+      type: String,
+      default: 'uncategorized',
+    },
+    branch: {
       type: String,
       default: 'uncategorized',
     },
