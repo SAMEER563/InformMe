0178ff0fed51a89d0cba15f3dcdb859186883b90
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-05-04 23:22:42 +0530

    Header change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -57,7 +57,7 @@ export default function Header() {
         className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
       >
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-          Fire 
+          RITM 
         </span>
         Blog
       </Link>
@@ -119,8 +119,8 @@ export default function Header() {
         <Navbar.Link active={path === '/about'} as={'div'}>
           <Link to='/about'>About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/projects'} as={'div'}>
-          <Link to='/projects'>Projects</Link>
+        <Navbar.Link active={path === '/contactus'} as={'div'}>
+          <Link to='/contactus'>Contact Us</Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
