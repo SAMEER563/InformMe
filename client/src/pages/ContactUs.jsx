dd4962da7ac20f1f6e58062ba95cdb0c1b48c744
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-05-04 23:23:17 +0530

    Contact Us page create

---
+++ client/src/pages/ContactUs.jsx
@@ -0,0 +1,15 @@
+
+
+export default function ContactUs() {
+  return (
+    <>
+   <div className="m-3 p-3">
+   <div className="text-pink-500 ">
+      Mobile Number - +91- 1234567890
+
+   </div>
+   <div className="text-blue-500"> Email - ritmlucknow@gmail.com</div>
+   </div>
+   </>
+  ) 
+}
