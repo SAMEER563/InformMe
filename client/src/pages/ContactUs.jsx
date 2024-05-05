658b90d1910d6c045f7081e765ce35d5f384eb72
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-05-05 22:39:05 +0530

    contact us page change

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -3,13 +3,14 @@
 export default function ContactUs() {
   return (
     <>
-   <div className="m-3 p-3">
+   <div className="m-3 p-3 flex flex-row gap-8 justify-between">
    <div className="text-pink-500 ">
       Mobile Number - +91- 1234567890
 
    </div>
    <div className="text-blue-500"> Email - ritmlucknow@gmail.com</div>
    </div>
+   
    </>
   ) 
 }
