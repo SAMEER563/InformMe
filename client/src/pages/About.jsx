faa88e3888e335c4d52453dc1cf5e59e3523d23f
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-05-05 22:38:52 +0530

    about page change

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,21 +5,17 @@ export default function About() {
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Fire Blog
+            About RITM Blog
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Fire Blog! This blog was created by Sameer Kumar Maurya
-              as a personal project to share his thoughts and ideas with the
-              world. Sameer is a passionate developer who loves to write about
+              Welcome to RITM Blog! This blog was created by Sameer Kumar Maurya
+              as a personal project to show all the information of college in a single website. Sameer is a passionate developer who loves to write about
               technology, coding, and everything in between.
             </p>
 
             <p>
-              On this blog, you&apos;ll find weekly articles and tutorials on topics
-              such as web development, software engineering, and programming
-              languages. Sameer is always learning and exploring new
-              technologies, so be sure to check back often for new content! 
+              On this blog, you&apos;ll find informations of college which is related to the placement, training, exams, carrer and many more.
             </p>
 
             <p>
