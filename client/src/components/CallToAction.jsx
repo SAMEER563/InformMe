a89e802dfcf0d61cdbf03b28dc0abca5d84b88ed
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-05-06 22:30:31 +0530

    call to action change

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -10,11 +10,11 @@ export default function CallToAction() {
             <p className='text-gray-500 my-2'>
                 Checkout these resources with 100 JavaScript Projects
             </p>
-            <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
+            {/* <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
                 <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
                     100 JavaScript Projects
                 </a>
-            </Button>
+            </Button> */}
         </div>
         <div className="p-7 flex-1">
             <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
