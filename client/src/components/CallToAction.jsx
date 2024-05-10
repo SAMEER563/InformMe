04539697814eeb65bf8b0d25105a8dafcbf77baf
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-05-10 22:29:32 +0530

    change

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -15,6 +15,7 @@ export default function CallToAction() {
                     100 JavaScript Projects
                 </a>
             </Button> */}
+            
         </div>
         <div className="p-7 flex-1">
             <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
