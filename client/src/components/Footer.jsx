3562a0687f2fcce9caee36ebe30c3dd416986b90
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-05-12 22:52:21 +0530

    Footer change

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -11,8 +11,8 @@ export default function FooterCom() {
                <div className="mt-5">
                <Link to="/" className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white">
        {/* <img src={header} alt="Header" /> */}
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">RITM</span>
-       Blog 
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">Inform</span>
+       Me 
        </Link>
                </div>
           
